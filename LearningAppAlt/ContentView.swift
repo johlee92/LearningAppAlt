a5d8ebacb9a27c2536f60314db35c314796fd251
John <johnathanlee72@hotmail.com>
2022-04-25 15:29:50 -0700

    Initial Commit

---
+++ LearningAppAlt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningAppAlt
+//
+//  Created by Johnathan Lee on 4/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningAppAlt/LearningAppAltApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppAltApp.swift
+//  LearningAppAlt
+//
+//  Created by Johnathan Lee on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppAltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
